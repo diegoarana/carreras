d44f64d58538eb5361561fedc011a92038333a5f
Diego Arana <diegoarana3@gmail.com>
2016-11-03 11:59:33 -0300

    Modificación de lógica de registrar usuario

--- carreras/form.py
+++ carreras/form.py
@@ -1,10 +1,66 @@
 from django import forms
+from django.contrib.auth.models import User
+from appcarreras.models import Usuario
+from django.contrib.auth import (
+    authenticate, get_user_model, password_validation,
+)
+from django.core.validators import RegexValidator
+from django.utils.translation import ugettext, ugettext_lazy as _
+
 
 class LoginForm(forms.Form):
 	username = forms.CharField()
 	password = forms.CharField(widget=forms.PasswordInput())
 
-class RegistrarForm(forms.Form):
-	username = forms.CharField()
-	password = forms.CharField(widget=forms.PasswordInput())
-	email = forms.EmailField()
+#class RegistrarForm(forms.Form):
+#	username = forms.CharField()
+#	password = forms.CharField(widget=forms.PasswordInput())
+#	email = forms.EmailField()
+
+class UserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
+    class Meta:
+        model = User
+        fields = ['username']
+        #field_classes = {'username': UsernameField}
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreationForm, self).__init__(*args, **kwargs)
+        self.fields[self._meta.model.USERNAME_FIELD].widget.attrs.update({'autofocus': ''})
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        self.instance.username = self.cleaned_data.get('username')
+        password_validation.validate_password(self.cleaned_data.get('password2'), self.instance)
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- carreras/views.py
+++ carreras/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from carreras.form import LoginForm, RegistrarForm
+from carreras.form import LoginForm, UserCreationForm, UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from appcarreras.models import Usuario
@@ -7,31 +7,23 @@ from appcarreras.models import Usuario
 def homepage(request):
 	return render(request, 'index.html')
 
+
 def registrar(request):
 	message = None
 	if request.method == 'POST':
-		form = RegistrarForm(request.POST)
-		if form.is_valid():
-			username = request.POST['username']
-			password = request.POST['password']
-			email = request.POST['email']
-			user = User.objects.create_user(username, email, password)
-			if user is not None:
-				user_auth = authenticate(username=username, password=password)
-				usuario = Usuario()
-				usuario.user = user
-				usuario.save()
-				if user.is_active:
-					login(request, user_auth)
-					message = "Te has logueado correctamente"
-					return redirect('homepage')
-				else:
-					message = "Tu usuario esta inactivo"
-			else:
-				message = "Nombre de usuario y/o password incorrecto"
+		form_user = UserCreationForm(request.POST or None)
+		if form_user.is_valid():
+			user = form_user.save()
+			usuario = Usuario(user=user)
+			usuario.save()
+			password = request.POST['password1']
+			user_auth = authenticate(username=user.username, password=password)
+			login(request, user_auth)
+			message = "Te has logueado correctamente"
+			return redirect('homepage')	
 	else:
-		form = RegistrarForm()
-	return render(request, 'registrar.html', {'message':message,'form':form})
+		form_user = UserCreationForm(request.POST or None)
+	return render(request, "registrar.html", {'form_user':form_user, 'message':message })
 
 def login_page(request):
 	message = None
@@ -57,3 +49,32 @@ def login_page(request):
 def logout_page(request):
 	logout(request)
 	return redirect('login_page')
+
+
+"""
+def registrar(request):
+	message = None
+	if request.method == 'POST':
+		form = RegistrarForm(request.POST)
+		if form.is_valid():
+			username = request.POST['username']
+			password = request.POST['password']
+			email = request.POST['email']
+			user = User.objects.create_user(username, email, password)
+			if user is not None:
+				user_auth = authenticate(username=username, password=password)
+				usuario = Usuario()
+				usuario.user = user
+				usuario.save()
+				if user.is_active:
+					login(request, user_auth)
+					message = "Te has logueado correctamente"
+					return redirect('homepage')
+				else:
+					message = "Tu usuario esta inactivo"
+			else:
+				message = "Nombre de usuario y/o password incorrecto"
+	else:
+		form = RegistrarForm()
+	return render(request, 'registrar.html', {'message':message,'form':form})
+"""
